e221cbecc30f37fed5647fc7be2fed8668eaaeb9
Yung Siang Liau <liauys@gmail.com>
2020-05-23 19:56:04 +0800

    Refactor into modules

---
+++ src/git.rs
@@ -0,0 +1,144 @@
+use std::fmt;
+
+use git2::Repository;
+use git2::BranchType;
+use git2::Branch;
+
+use chrono::NaiveDateTime;
+use chrono::offset::FixedOffset;
+use chrono::offset::TimeZone;
+use chrono_humanize::HumanTime;
+
+pub struct BranchRecord {
+    pub name: String,
+    pub commit_sha: String,
+    pub time_seconds: i64,
+    pub offset_minutes: i32,
+    pub summary: String,
+    pub ref_name: String,
+    pub author_name: String,
+    pub is_current_branch: bool,
+}
+
+impl BranchRecord {
+    pub fn pretty_format_date(&self) -> String {
+        let naive_dt = NaiveDateTime::from_timestamp(self.time_seconds, 0);
+        let offset = FixedOffset::east(self.offset_minutes * 60);
+        let dt = offset.from_utc_datetime(&naive_dt);
+        let humanized_dt = HumanTime::from(dt);
+        humanized_dt.to_string()
+    }
+}
+
+impl fmt::Display for BranchRecord {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Branch({}, {}, {}, {})",
+            self.name,
+            self.commit_sha,
+            self.summary,
+            self.pretty_format_date(),
+        )
+    }
+}
+
+fn parse_local_branch(branch: &Branch, head_branch_refname: &Option<String>) -> Option<BranchRecord> {
+
+    let mut is_valid = true;
+
+    let mut branch_name = String::from("unknown");
+    let mut commit_sha = String::from("unknown");
+    let mut time_seconds = 0;
+    let mut offset_minutes = 0;
+    let mut summary = String::from("unknown");
+    let mut author_name = String::from("unknown");
+    let mut is_current_branch = false;
+
+    match branch.name() {
+        Ok(name) => if let Some(name) = name {
+            branch_name = name.to_string();
+        },
+        Err(e) => {
+            println!("branch name error: {}", e);
+            is_valid = false;
+        },
+    };
+
+    let reference = branch.get();
+
+    let ref_name = reference.name()?.to_string();
+    if let Some(current) = head_branch_refname {
+        is_current_branch = ref_name == current.as_str();
+    }
+
+    match reference.peel_to_commit() {
+        Ok(commit) => {
+            commit_sha = commit.id().to_string();
+            time_seconds = commit.time().seconds();
+            offset_minutes = commit.time().offset_minutes();
+            if let Some(s) = commit.summary() {
+                summary = s.to_string();
+            }
+            author_name = commit.author().name()?.to_string();
+        },
+        Err(e) => {
+            println!("error getting commit: {}", e);
+            is_valid = false;
+        },
+    }
+
+    if is_valid {
+        let record = BranchRecord {
+            name: branch_name,
+            commit_sha,
+            time_seconds,
+            offset_minutes,
+            summary,
+            ref_name,
+            author_name,
+            is_current_branch,
+        };
+        Some(record)
+    } else {
+        None
+    }
+
+}
+
+fn get_current_branch_refname(repo: &Repository) -> Option<String> {
+    if let Ok(is_detached) = repo.head_detached() {
+        if is_detached {
+            return None
+        }
+    };
+    if let Ok(head) = repo.head() {
+        if let Some(name) = head.name() {
+            return Some(name.to_string())
+        }
+    };
+    None
+}
+
+pub fn extract_local_branches(repo: &Repository) -> Vec<BranchRecord> {
+
+    let mut records: Vec<BranchRecord> = Vec::new();
+
+    let current_branch_refname = get_current_branch_refname(repo);
+
+    match repo.branches(Some(BranchType::Local)) {
+        Ok(branches) => for branch in branches {
+            match branch {
+                Ok((branch, _)) => {
+                    if let Some(record) = parse_local_branch(&branch, &current_branch_refname) {
+                        records.push(record)
+                    }
+                }
+                Err(e) => println!("error in branch: {}", e),
+            }
+        }
+        Err(e) => panic!("failed to get branches: {}", e),
+    };
+
+    records
+}
---
+++ src/ui.rs
@@ -0,0 +1,155 @@
+use tui::{
+    backend::TermionBackend,
+    layout::{Constraint, Layout},
+    style::{Color, Modifier, Style},
+    widgets::{Block, Borders, Row, Table, TableState},
+    Terminal,
+};
+use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
+use std::{error::Error, io};
+
+use super::git::BranchRecord;
+use super::util::event::{Event, Events};
+
+pub struct BranchTable<'a> {
+    state: TableState,
+    items: Vec<Vec<String>>,
+    header: Vec<String>,
+    records: &'a Vec<BranchRecord>,
+}
+
+impl<'a> BranchTable<'a> {
+    pub fn new(records: &'a Vec<BranchRecord>) -> BranchTable<'a> {
+        let (data, header) = get_table_data_from_branch_records(&records);
+        BranchTable {
+            state: TableState::default(),
+            items: data,
+            header: header,
+            records: records,
+        }
+    }
+
+    pub fn init(&mut self) {
+        self.state.select(Some(0));
+    }
+
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i + 3 >= self.items.len() {
+                    i
+                } else {
+                    i + 3
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i < 3 {
+                    i
+                } else {
+                    i - 3
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn deselect(&mut self) {
+        self.state.select(None);
+    }
+
+    pub fn selected_record(&mut self) -> Option<&BranchRecord> {
+        match self.state.selected() {
+            Some(row) => self.records.get(row / 3),
+            _ => None,
+        }
+    }
+}
+
+fn get_table_data_from_branch_records(records: &[BranchRecord]) -> (Vec<Vec<String>>, Vec<String>) {
+    let mut data = vec![];
+    let header = vec![String::from("Name"), String::from("Last Commit")];
+    for r in records {
+        let mut name = r.name.clone();
+        if r.is_current_branch {
+            name = String::from("* ") + &name;
+        }
+        let commit_info = format!(
+            "{} ({}) {}", &r.commit_sha[..8], r.pretty_format_date(), r.author_name
+        );
+        let row = vec![name, commit_info.clone()];
+        data.push(row);
+        let row = vec![String::from(""), r.summary.clone()];
+        data.push(row);
+        let row = vec![String::from(""), String::from("")];
+        data.push(row);
+    }
+    (data, header)
+}
+
+pub fn render_branch_selection<'a>(table: &'a mut BranchTable) -> Result<Option<&'a BranchRecord>, Box<dyn Error>> {
+    // Terminal initialization
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
+
+    let events = Events::new();
+
+    table.init();
+
+    // Input
+    loop {
+        terminal.draw(|mut f| {
+            let rects = Layout::default()
+                .constraints([Constraint::Percentage(100)].as_ref())
+                .margin(5)
+                .split(f.size());
+
+            let selected_style = Style::default().fg(Color::Yellow).modifier(Modifier::BOLD);
+            let normal_style = Style::default().fg(Color::White);
+            let rows = table
+                .items
+                .iter()
+                .map(|i| Row::StyledData(i.iter(), normal_style));
+            let t = Table::new(table.header.iter(), rows)
+                .block(Block::default().borders(Borders::ALL).title("Recent branches"))
+                .highlight_style(selected_style)
+                .highlight_symbol(">> ")
+                .widths(&[
+                    Constraint::Percentage(20),
+                    Constraint::Percentage(80),
+                ]);
+            f.render_stateful_widget(t, rects[0], &mut table.state);
+        })?;
+
+        if let Event::Input(key) = events.next()? {
+            match key {
+                Key::Char('q') => {
+                    table.deselect();
+                }
+                Key::Down => {
+                    table.next();
+                }
+                Key::Up => {
+                    table.previous();
+                }
+                Key::Char('\n') => {
+                    break;
+                }
+                _ => {}
+            }
+        };
+    }
+
+    Ok(table.selected_record())
+}
